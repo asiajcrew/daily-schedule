9166058b98a4331a5fa16a938b1239c094f71786
asiajcrew <asia_jcrew96@yahoo.com>
2021-12-18 14:36:04 -0500

    added local storage functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,43 @@
+var tasks = {};
+
+// get & set task to local storage
 $(document).ready(function() {
-    // select the save button
+  for (i = 9; i < 18; i++) {
+    var currentHour = "" + i + "";
+    var currentHourText = localStorage.getItem(currentHour);
+    if (currentHourText) {
+      document.getElementById(currentHour).textContent = currentHourText;
+      console.log(currentHourText);
+    }
+  };
+  // select the save button
     var saveButton = $(".saveBtn")
     // add event listener
     saveButton.on("click", function(event) {
         event.preventDefault();
+        // get user input from html
         var userTask = $(this).siblings("textarea").val();
         var hour = $(this).siblings("textarea").attr("id");
+        // save task to local storage
         localStorage.setItem(hour, userTask);
-
     });
-    // get user input from html
-    // save task to local storage
+});
 
-})
+// pull tasks from local storage
 
 // display the current date
-var weekday = ["Sunday", "Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
-var month = ["January","February","March","April","May","June","July","August","September","October","November","December"];
-var day = ["1st", "2nd", "3rd", "4th", "5th", "6th", "7th", "8th", "9th", "10th", "11th", "12th", "13th", "14th", "15th", "16th", "17th", "18th", "19th", "20th", "21st", "22nd", "23rd", "24th", "25th", "26th", "27th", "28th", "29th", "30th", "31st"]
-var date = new Date();
+var date = document.getElementById("currentDay");
+date.textContent = moment().format("dddd, MMMM Do YYYY");
 
-let currentDay = weekday[date.getDay()];
-let currentMonth = month[date.getMonth()];
-let currentDate = day[date.getDate()];
-document.getElementById("currentDay").innerHTML = currentDay + ", " + currentMonth + " " + currentDate;
+// check if current time is in the past, present, or future
+
+  // get date from task element
+
+  // remove any old classes from element
+
+  // apply new class if task is over due date
+//   if
+//   } else if
+// };
+
+  // loop over object properties
